73ce0a1d863d445a7d1e352e63d6b6550fdec13c
thecsharper <si@thecsharper.com>
2022-02-12 17:39:23 +0000

    Initial commit

---
+++ AsyncVoidSynchronizationContext.cs
@@ -0,0 +1,71 @@
+﻿namespace AsyncVoidDemo
+{
+    public sealed class AsyncVoidSynchronizationContext : SynchronizationContext
+    {
+        private static readonly SynchronizationContext s_default = new SynchronizationContext();
+
+        private readonly SynchronizationContext _innerSynchronizationContext;
+        private readonly TaskCompletionSource _tcs = new();
+        private int _startedOperationCount;
+
+        public AsyncVoidSynchronizationContext(SynchronizationContext? innerContext)
+        {
+            _innerSynchronizationContext = innerContext ?? s_default;
+        }
+
+        public Task Completed => _tcs.Task;
+
+        public override void OperationStarted()
+        {
+            Interlocked.Increment(ref _startedOperationCount);
+        }
+
+        public override void OperationCompleted()
+        {
+            if (Interlocked.Decrement(ref _startedOperationCount) == 0)
+            {
+                _tcs.TrySetResult();
+            }
+        }
+
+        public override void Post(SendOrPostCallback d, object? state)
+        {
+            Interlocked.Increment(ref _startedOperationCount);
+
+            try
+            {
+                _innerSynchronizationContext.Post(s =>
+                {
+                    try
+                    {
+                        d(s);
+                    }
+                    catch (Exception ex)
+                    {
+                        _tcs.TrySetException(ex);
+                    }
+                    finally
+                    {
+                        OperationCompleted();
+                    }
+                }, state);
+            }
+            catch (Exception ex)
+            {
+                _tcs.TrySetException(ex);
+            }
+        }
+
+        public override void Send(SendOrPostCallback d, object? state)
+        {
+            try
+            {
+                _innerSynchronizationContext.Send(d, state);
+            }
+            catch (Exception ex)
+            {
+                _tcs.TrySetException(ex);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// From: https://www.meziantou.net/awaiting-an-async-void-method-in-dotnet.htm
+
+namespace AsyncVoidDemo
+{
+    public class Program
+    {
+        public async static Task Main()
+        {
+            Console.WriteLine("before");
+            
+            await Run(() => Test());
+            
+            Console.WriteLine("after");
+        }
+
+       public static async void Test()
+        {
+            Console.WriteLine("begin");
+
+            await Task.Delay(1000);
+            
+            Console.WriteLine("end");
+        }
+
+        public static async Task Run(Action action)
+        {
+            var currentContext = SynchronizationContext.Current;
+            var synchronizationContext = new AsyncVoidSynchronizationContext(currentContext);
+            SynchronizationContext.SetSynchronizationContext(synchronizationContext);
+            try
+            {
+                action();
+
+                // Wait for the async void method to call OperationCompleted or to report an exception
+                await synchronizationContext.Completed;
+            }
+            finally
+            {
+                // Reset the original SynchronizationContext
+                SynchronizationContext.SetSynchronizationContext(currentContext);
+            }
+        }
+    }
+}
